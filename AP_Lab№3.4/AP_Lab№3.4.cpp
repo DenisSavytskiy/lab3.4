2ef6248e93709d3e401c9d2eb222a7057dd70c20
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2024-09-18 21:41:27 +0300

    lab3.4

---
+++ AP_Lab№3.4/AP_Lab№3.4.cpp
@@ -0,0 +1,25 @@
+﻿// Lab_03_4.cpp
+// Савицький Денис
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 27
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x; // вхідний аргумент
+    double y; // вхідний параметр
+    double R; // вхідний параметр
+
+    cout << "R = ";cin >> R;
+    cout << "x = ";cin >> x;
+    cout << "y = ";cin >> y;
+
+    // розгалуження в повній формі
+    if ((x>=-2*R && x<=0 && y>=0 && y<=2*R && x*x+(y*y)<=R*R) || (x>=0 && x<=2*R && y>=-R && y<=0)) cout << "yes" << endl;
+    else cout << "no" << endl;
+
+    return 0;
+}
